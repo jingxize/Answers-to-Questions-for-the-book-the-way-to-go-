d89637df00a74e77e6c416e81c0e7a230909d462
sanshan <jingxize@hotmail.com>
2017-06-23 18:55:53 +0800

    answers

--- answers/chapter_7/8.1.go
+++ answers/chapter_7/8.1.go
@@ -1,32 +1,32 @@
 /*
 创建一个 map 来保存每周 7 天的名字，将它们打印出来并且测试是否存在 Tuesday 和 Hollyday
- */
+*/
 package main
 
 func main() {
 	week := map[string]string{
-		"one":"Sunday",
-		"two":"Monday",
-		"three":"Tuesday",
-		"four":"Wednesday",
-		"five":"Thursday",
-		"six":"Friday",
-		"seven":"Saturday",
+		"one":   "Sunday",
+		"two":   "Monday",
+		"three": "Tuesday",
+		"four":  "Wednesday",
+		"five":  "Thursday",
+		"six":   "Friday",
+		"seven": "Saturday",
 	}
-	for _,x := range week{
+	for _, x := range week {
 		print(x)
 		print("\n")
 	}
 
 	print("\n")
 
-	for _,ok  := range week{
-		if ok == "Tuesday"{
+	for _, ok := range week {
+		if ok == "Tuesday" {
 			print("Tuesday is find in map of week")
 			print("\n")
 		}
 
-		if ok == "Hollyday"{
+		if ok == "Hollyday" {
 			print("Hollyday is find in map of week")
 		}
 	}
---
+++ answers/chapter_7/8.2.go
@@ -0,0 +1,49 @@
+/*
+构造一个将英文饮料名映射为法语（或者任意你的母语）的集合；先打印所有的饮料，然后打印原名和翻译后的名字。接下来按照英文名排序后再打印出来。
+备注：小弟不才，因为饮料翻译比较困难，所以改为了水果
+*/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fruit := map[string]string{"peach": "桃子", "Lemon": "柠檬", "Pear": "梨子"}
+	for key, val := range fruit {
+		fmt.Printf("%s:%s", key, val)
+		fmt.Printf("\n")
+	}
+
+	fmt.Printf("\n")
+
+	for key := range fruit {
+		fmt.Printf(key)
+		fmt.Printf("\n")
+	}
+
+	fmt.Printf("\n")
+
+	for _, val := range fruit {
+		fmt.Printf(val)
+		fmt.Printf("\n")
+	}
+
+	fmt.Printf("\n")
+
+	arr := make([]string, len(fruit))
+	i := 0
+	for key := range fruit {
+		arr[i] = key
+		i++
+	}
+
+	sort.Strings(arr)
+
+	for _, val := range arr {
+		fmt.Printf("%s:%s", val, fruit[val])
+		fmt.Printf("\n")
+	}
+
+}
